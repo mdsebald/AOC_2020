36f5b02fb45b3077332c5735b6c5b9ae6f4165d7
mdsebald <mdsebald@gmail.com>
2020-12-03 08:56:27 -0600

    Added tests

--- test/aoc2020_test.exs
+++ test/aoc2020_test.exs
@@ -1,8 +1,24 @@
 defmodule Aoc2020Test do
   use ExUnit.Case
-  doctest Aoc2020
 
-  test "greets the world" do
-    assert Aoc2020.hello() == :world
+  test "Day 01 fix_expense_report" do
+    assert Day01.fix_expense_report
   end
+
+  test "Day 02 count_valid_sled_passwords" do
+    assert Day02.count_valid_sled_passwords == 548
+  end
+
+  test "Day 02 count_valid_toboggan_passwords" do
+    assert Day02.count_valid_toboggan_passwords == 502
+  end
+
+  test "Day 03 tree_encounter" do
+    assert Day03.tree_encounter == 209
+  end
+
+  test "Day 03 check_multiple_slopes" do
+    assert Day03.check_multiple_slopes == 1574890240
+  end
+
 end
