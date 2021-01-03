ba261638dcf472405853f2580f66e74d6cb63db3
mdsebald <mdsebald@gmail.com>
2021-01-03 07:03:38 -0600

    Reformat @doc. Add puzzle answers

--- lib/day04.ex
+++ lib/day04.ex
@@ -6,19 +6,34 @@ defmodule Day04 do
   @doc """
   --- Part One ---
 
-  Detect which passports have all required fields. The expected fields are as follows:
+  You arrive at the airport only to realize that you grabbed your North Pole
+  Credentials instead of your passport. While these documents are extremely
+  similar, North Pole Credentials aren't issued by a country and therefore
+  aren't actually valid documentation for travel in most of the world.
 
-  byr (Birth Year)
-  iyr (Issue Year)
-  eyr (Expiration Year)
-  hgt (Height)
-  hcl (Hair Color)
-  ecl (Eye Color)
-  pid (Passport ID)
-  cid (Country ID)
-  Passport data is validated in batch files (your puzzle input).
-  Each passport is represented as a sequence of key:value pairs separated by spaces or newlines.
-  Passports are separated by blank lines.
+  It seems like you're not the only one having problems, though; a very long
+  line has formed for the automatic passport scanners, and the delay could
+  upset your travel itinerary.
+
+  Due to some questionable network security, you realize you might be able to
+  solve both of these problems at the same time.
+
+  The automatic passport scanners are slow because they're having trouble
+  detecting which passports have all required fields. The expected fields are
+  as follows:
+
+    - byr (Birth Year)
+    - iyr (Issue Year)
+    - eyr (Expiration Year)
+    - hgt (Height)
+    - hcl (Hair Color)
+    - ecl (Eye Color)
+    - pid (Passport ID)
+    - cid (Country ID)
+
+  Passport data is validated in batch files (your puzzle input). Each
+  passport is represented as a sequence of key:value pairs separated by
+  spaces or newlines. Passports are separated by blank lines.
 
   Here is an example batch file containing four passports:
 
@@ -35,17 +50,27 @@ defmodule Day04 do
 
   hcl:#cfa07d eyr:2025 pid:166559648
   iyr:2011 ecl:brn hgt:59in
-  The first passport is valid - all eight fields are present. The second passport is invalid - it is missing hgt (the Height field).
 
-  The third passport is interesting; the only missing field is cid, so it looks like data from North Pole Credentials, not a passport at all!
-  Surely, nobody would mind if you made the system temporarily ignore missing cid fields. Treat this "passport" as valid.
+  The first passport is valid - all eight fields are present. The second
+  passport is invalid - it is missing hgt (the Height field).
 
-  The fourth passport is missing two fields, cid and byr. Missing cid is fine, but missing any other field is not, so this passport is invalid.
+  The third passport is interesting; the only missing field is cid, so it
+  looks like data from North Pole Credentials, not a passport at all! Surely,
+  nobody would mind if you made the system temporarily ignore missing cid
+  fields. Treat this "passport" as valid.
 
-  According to the above rules, your improved system would report 2 valid passports.
+  The fourth passport is missing two fields, cid and byr. Missing cid is
+  fine, but missing any other field is not, so this passport is invalid.
 
-  Count the number of valid passports - those that have all required fields. Treat cid as optional. In your batch file, how many passports are valid?
+  According to the above rules, your improved system would report 2 valid
+  passports.
+
+  Count the number of valid passports - those that have all required fields.
+  Treat cid as optional. In your batch file, how many passports are valid?
+
+  Your puzzle answer was 245.
   """
+
   def count_valid_passports_1 do
     get_passports()
     |> Enum.reduce(0, fn pp_map, count ->
@@ -65,22 +90,27 @@ defmodule Day04 do
   @doc """
   --- Part Two ---
 
-  Add some data validation.
+  The line is moving more quickly now, but you overhear airport security
+  talking about how passports with invalid data are getting through.
+  Better add some data validation, quick!
 
-  You can continue to ignore the cid field, but each other field has strict rules about what values are valid for automatic validation:
+  You can continue to ignore the cid field, but each other field has strict
+  rules about what values are valid for automatic validation:
 
-  byr (Birth Year) - four digits; at least 1920 and at most 2002.
-  iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-  eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
-  hgt (Height) - a number followed by either cm or in:
-  If cm, the number must be at least 150 and at most 193.
-  If in, the number must be at least 59 and at most 76.
-  hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-  ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-  pid (Passport ID) - a nine-digit number, including leading zeroes.
-  cid (Country ID) - ignored, missing or not.
-  Your job is to count the passports where all required fields are both present and valid according to the above rules.
-  Here are some example values:
+    - byr (Birth Year) - four digits; at least 1920 and at most 2002.
+    - iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+    - eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+    - hgt (Height) - a number followed by either cm or in:
+        - If cm, the number must be at least 150 and at most 193.
+        - If in, the number must be at least 59 and at most 76.
+    - hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+    - ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+    - pid (Passport ID) - a nine-digit number, including leading zeroes.
+    - cid (Country ID) - ignored, missing or not.
+
+  Your job is to count the passports where all required fields are both
+  present and valid according to the above rules. Here are some example
+  values:
 
   byr valid:   2002
   byr invalid: 2003
@@ -99,6 +129,7 @@ defmodule Day04 do
 
   pid valid:   000000001
   pid invalid: 0123456789
+
   Here are some invalid passports:
 
   eyr:1972 cid:100
@@ -114,6 +145,7 @@ defmodule Day04 do
   hgt:59cm ecl:zzz
   eyr:2038 hcl:74454a iyr:2023
   pid:3556412378 byr:2007
+
   Here are some valid passports:
 
   pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
@@ -128,8 +160,14 @@ defmodule Day04 do
   eyr:2022
 
   iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
-  Count the number of valid passports - those that have all required fields and valid values. Continue to treat cid as optional.
+
+  Count the number of valid passports - those that have all required fields
+  and valid values. Continue to treat cid as optional. In your batch file,
+  how many passports are valid?
+
+  Your puzzle answer was 133.
   """
+
   def count_valid_passports_2 do
     get_passports()
     |> Enum.reduce(0, fn pp_map, count ->
