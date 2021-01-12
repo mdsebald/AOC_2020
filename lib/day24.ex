d99b9980e80e6644d5ba539d5763c33ed28f8ad3
mdsebald <mdsebald@gmail.com>
2021-01-11 19:01:53 -0600

    Added puzzle answers to @doc

--- lib/day24.ex
+++ lib/day24.ex
@@ -6,37 +6,41 @@ defmodule Day24 do
   @doc """
   --- Part One ---
 
-  Your raft makes it to the tropical island; it turns out that the small crab was an excellent navigator.
-  You make your way to the resort.
+  Your raft makes it to the tropical island; it turns out that the small crab
+  was an excellent navigator. You make your way to the resort.
 
-  As you enter the lobby, you discover a small problem: the floor is being renovated.
-  You can't even reach the check-in desk until they've finished installing the new tile floor.
+  As you enter the lobby, you discover a small problem: the floor is being
+  renovated. You can't even reach the check-in desk until they've finished
+  installing the new tile floor.
 
-  The tiles are all hexagonal; they need to be arranged in a hex grid with a very specific color pattern.
-  Not in the mood to wait, you offer to help figure out the pattern.
+  The tiles are all hexagonal; they need to be arranged in a hex grid with a
+  very specific color pattern. Not in the mood to wait, you offer to help
+  figure out the pattern.
 
-  The tiles are all white on one side and black on the other. They start with the white side facing up.
-  The lobby is large enough to fit whatever pattern might need to appear there.
+  The tiles are all white on one side and black on the other. They start with
+  the white side facing up. The lobby is large enough to fit whatever pattern
+  might need to appear there.
 
-  A member of the renovation crew gives you a list of the tiles that need to be flipped over (your puzzle input).
-  Each line in the list identifies a single tile that needs to be flipped
-  by giving a series of steps starting from a reference tile in the very center of the room.
-  (Every line starts from the same reference tile.)
+  A member of the renovation crew gives you a list of the tiles that need to
+  be flipped over (your puzzle input). Each line in the list identifies a
+  single tile that needs to be flipped by giving a series of steps starting
+  from a reference tile in the very center of the room. (Every line starts
+  from the same reference tile.)
 
-  Because the tiles are hexagonal, every tile has six neighbors: east, southeast, southwest, west, northwest, and northeast.
-  These directions are given in your list, respectively, as e, se, sw, w, nw, and ne.
-  A tile is identified by a series of these directions with no delimiters; for example,
+  Because the tiles are hexagonal, every tile has six neighbors: east,
+  southeast, southwest, west, northwest, and northeast. These directions are
+  given in your list, respectively, as e, se, sw, w, nw, and ne. A tile is
+  identified by a series of these directions with no delimiters; for example,
   esenee identifies the tile you land on if you start at the reference tile
-  and then move one tile east, one tile southeast, one tile northeast, and one tile east.
+  and then move one tile east, one tile southeast, one tile northeast, and
+  one tile east.
 
-  Each time a tile is identified, it flips from white to black or from black to white.
-  Tiles might be flipped more than once. For example,
-  a line like esew flips a tile immediately adjacent to the reference tile,
-  and a line like nwwswee flips the reference tile itself.
+  Each time a tile is identified, it flips from white to black or from black
+  to white. Tiles might be flipped more than once. For example, a line like
+  esew flips a tile immediately adjacent to the reference tile, and a line
+  like nwwswee flips the reference tile itself.
 
   Here is a larger example:
-
-  se se nw ne ne ne w se e sw w sw sw w ne ne w se w sw
 
   sesenwnenenewseeswwswswwnenewsewsw
   neeenesenwnwwswnenewnwwsewnenwseswesw
@@ -59,17 +63,19 @@ defmodule Day24 do
   neswnwewnwnwseenwseesewsenwsweewe
   wseweeenwnesenwwwswnew
 
-  In the above example, 10 tiles are flipped once (to black),
-  and 5 more are flipped twice (to black, then back to white).
-  After all of these instructions have been followed, a total of 10 tiles are black.
+  In the above example, 10 tiles are flipped once (to black), and 5 more are
+  flipped twice (to black, then back to white). After all of these
+  instructions have been followed, a total of 10 tiles are black.
 
-  Go through the renovation crew's list and determine which tiles they need to flip.
-  After all of the instructions have been followed, how many tiles are left with the black side up?
+  Go through the renovation crew's list and determine which tiles they need
+  to flip. After all of the instructions have been followed, how many tiles
+  are left with the black side up?
+
+  Your puzzle answer was 512.
   """
 
   def count_black_side_up_tiles do
     tiles = %{{0, 0} => :white}
-    # "inputs/day24_test1_input.txt")
     get_tile_instr()
     |> Enum.reduce(tiles, fn path, new_tiles ->
       follow_path(path, new_tiles, {0, 0})
@@ -80,17 +86,23 @@ defmodule Day24 do
   @doc """
   --- Part Two ---
 
-  The tile floor in the lobby is meant to be a living art exhibit.
-  Every day, the tiles are all flipped according to the following rules:
+  The tile floor in the lobby is meant to be a living art exhibit. Every day,
+  the tiles are all flipped according to the following rules:
 
-  Any black tile with zero or more than 2 black tiles immediately adjacent to it is flipped to white.
-  Any white tile with exactly 2 black tiles immediately adjacent to it is flipped to black.
-  Here, tiles immediately adjacent means the six tiles directly touching the tile in question.
+    - Any black tile with zero or more than 2 black tiles immediately
+      adjacent to it is flipped to white.
+    - Any white tile with exactly 2 black tiles immediately adjacent to it
+      is flipped to black.
 
-  The rules are applied simultaneously to every tile; put another way,
-  it is first determined which tiles need to be flipped, then they are all flipped at the same time.
+  Here, tiles immediately adjacent means the six tiles directly touching the
+  tile in question.
 
-  In the above example, the number of black tiles that are facing up after the given number of days has passed is as follows:
+  The rules are applied simultaneously to every tile; put another way, it is
+  first determined which tiles need to be flipped, then they are all flipped
+  at the same time.
+
+  In the above example, the number of black tiles that are facing up after
+  the given number of days has passed is as follows:
 
   Day 1: 15
   Day 2: 12
@@ -112,14 +124,17 @@ defmodule Day24 do
   Day 80: 1373
   Day 90: 1844
   Day 100: 2208
-  After executing this process a total of 100 times, there would be 2208 black tiles facing up.
+
+  After executing this process a total of 100 times, there would be 2208
+  lack tiles facing up.
 
   How many tiles will be black after 100 days?
+
+  Your puzzle answer was 4120.
   """
 
   def flip_tiles_100_days do
     tiles = %{{0, 0} => :white}
-    # "inputs/day24_test1_input.txt")
     get_tile_instr()
     |> Enum.reduce(tiles, fn path, new_tiles ->
       follow_path(path, new_tiles, {0, 0})
